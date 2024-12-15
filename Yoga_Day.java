b20e81b6e7e6e51f8e9a7dab7d226c76a7f1c3d4
sv742744 <shashi09op@gmail.com>
2024-12-15 09:52:01 +0530

    Create Yoga_Day.java

---
+++ Yoga_Day.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    int n=s.nextInt();
+	    if(n>12)
+	        System.out.println(n/12);
+	    else 
+	        System.out.println("0");
+
+	}
+}
