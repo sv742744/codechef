9708f611c45e3175e08f0f1ce16f4911acb89d33
sv742744 <shashi09op@gmail.com>
2024-12-15 14:27:56 +0530

    Create Biryani_classes.java

---
+++ Biryani_classes.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=0;i<n;i++){
+		int x=s.nextInt();
+		int y=s.nextInt();
+		System.out.println(x*y);
+}
+	}
+}
