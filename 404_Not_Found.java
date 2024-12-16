9b242e102f2959067e473d2a04f9125f3a8ad702
sv742744 <shashi09op@gmail.com>
2024-12-16 12:07:48 +0530

    Create 404_Not_Found.java

---
+++ 404_Not_Found.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		if(x==404){
+		    System.out.println("NOT FOUND");
+		}
+		else{
+		    System.out.println("FOUND");
+		}
+
+	}
+}
